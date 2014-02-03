4b9ea62330ce22de295f66c1a0848742adce8d92
Alexander Skjolden <alex@plastboks.net>
2014-02-03 12:59:23 +0100

    install mysql without prompt
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-sudo apt-get install postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql mysql-server
+PASSWORD="abc123"
+
+sudo apt-get install postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd 
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install dovecot-mysql mysql-server
 

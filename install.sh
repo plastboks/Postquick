66ec18292f8611d5bf4290440a5fea1fc58d650a
Alexander Skjolden <alex@plastboks.net>
2014-02-03 14:12:42 +0100

    some changes
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 PASSWORD="abc123"
+MAILNAME="server.com"
+MAINMAILTYPE="Internet Site"
 
-sudo apt-get install postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd 
+# install postfix and dovecot
+sudo debconf-set-selections <<< "postfix postfix/mailname select $MAILNAME"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type select $MAINMAILTYPE"
+sudo apt-get -y install postfix dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd ntp
+
+# install mysql
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install dovecot-mysql mysql-server
+sudo apt-get -y install dovecot-mysql mysql-server postfix-mysql
 

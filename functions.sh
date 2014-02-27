707d8352ae0aa084487929ae67cc7ad0dbf9b0a3
Alexander Skjolden <alex@plastboks.net>
2014-02-27 22:06:29 +0100

    some commenting
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- functions.sh
+++ functions.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-# 
-# A handy function found @ the interwebs
+#
+# Some functions for Postquick
+#
+
+
+# Random string generation.
+# Function found @ the interwebs
 # http://utdream.org/post.cfm/bash-generate-a-random-string
-
 function randomstring {
-        # if a param was passed, it's the length of the string we want
-        if [[ -n $1 ]] && [[ "$1" -lt 20 ]]; then
-                local myStrLength=$1;
-        else
-                # otherwise set to default
-                local myStrLength=8;
-        fi
+    # if a param was passed, it's the length of the string we want
+    # otherwise set to default
+    if [[ -n $1 ]] && [[ "$1" -lt 20 ]]; then
+        local myStrLength=$1;
+    else
+        local myStrLength=8;
+    fi
 
-        local mySeedNumber=$$`date +%N`; # seed will be the pid + nanoseconds
-        local myRandomString=$( echo $mySeedNumber | md5sum | md5sum );
-        # create our actual random string
-        myRandomResult="${myRandomString:2:myStrLength}"
+    local mySeedNumber=$$`date +%N`; # seed will be the pid + nanoseconds
+    local myRandomString=$( echo $mySeedNumber | md5sum | md5sum );
+    # create our actual random string
+    myRandomResult="${myRandomString:2:myStrLength}"
 }
